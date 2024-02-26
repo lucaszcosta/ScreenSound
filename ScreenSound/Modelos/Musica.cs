907e1e1de339d371aabe6d3ea06dbfc2aeb3260e
lucas costa <lucaszcosta@hotmail.com.br>
2024-02-26 11:48:02 -0300

    Adicionado referencias ao json

---
+++ ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace ScreenSound.Model;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Musica: {Nome}");
+        Console.WriteLine($"Duracao: {Duracao/1000}");
+        Console.WriteLine($"Genero: {Genero}");
+    }
+}
