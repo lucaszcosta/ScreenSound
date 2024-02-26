67f81c40c2c247efef67e8e0bf471c0afa022b94
lucas costa <lucaszcosta@hotmail.com.br>
2024-02-26 17:04:27 -0300

    Contagem de musicas

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,10 +1,17 @@
 ﻿
+using ScreenSound.Model;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
         Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+        Console.WriteLine(musicas.Count);
+        
     }
     catch (Exception ex)
     {
