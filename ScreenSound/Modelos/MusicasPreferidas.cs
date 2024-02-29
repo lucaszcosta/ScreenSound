ab46743e905d285653187b77fa11474aeb77d5c5
lucas costa <lucaszcosta@hotmail.com.br>
2024-02-29 10:57:18 -0300

    Adicionado musicas favoritas

---
+++ ScreenSound/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,36 @@
+﻿using ScreenSound.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Modelos
+{
+    internal class MusicasPreferidas
+    {
+        public string? Nome { get; set; }
+        public List<Musica> ListaDeMusicasFavoritas;
+
+
+        public MusicasPreferidas(string nome) {
+
+            Nome = nome;
+            ListaDeMusicasFavoritas = new List<Musica>();
+        }
+
+        public void AdicionarMusicasFavoritas(Musica musica)
+        {
+            ListaDeMusicasFavoritas.Add(musica);
+        }
+        
+        public void ExibirMusicasFavoritas()
+        {
+            Console.WriteLine($"Essas são as musicas favoritas -> {Nome}");
+            foreach(var musica in ListaDeMusicasFavoritas)
+            {
+                Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+            }
+        }
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScreenSound.Filtros;
 using ScreenSound.Model;
+using ScreenSound.Modelos;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,10 +9,22 @@ using (HttpClient client = new HttpClient())
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        //Console.WriteLine(resposta);
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
-        LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "Rock");
-        
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "Michel Teló");
+
+
+        var minhasMusicasPreferidas = new MusicasPreferidas("Lucas");
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[999]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[38]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[400]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[333]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[39]);
+        minhasMusicasPreferidas.AdicionarMusicasFavoritas(musicas[403]);
+
+        minhasMusicasPreferidas.ExibirMusicasFavoritas();
+
     }
     catch (Exception ex)
     {
